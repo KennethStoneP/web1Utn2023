06dc7a64985edc46b0658a0800f098c434e3ac6a
KennethStoneP <kennethpv1450@gmail.com>
2023-04-12 20:08:25 -0600

    UltimaTarea

---
+++ animacionsPaginas/animaciones/animacion2.js
@@ -0,0 +1,19 @@
+ 
+window.addEventListener("load", function() {
+  document.body.style.opacity = "1";
+});
+
+  window.onload = function() {
+    const loader = document.querySelector('.loader');
+    const content = document.querySelector('.content');
+  
+    setTimeout(() => {
+      loader.classList.add('hide');
+      content.classList.remove('hide');
+  
+
+      
+      
+    }, 2000);
+  }
+ 
